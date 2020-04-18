ff47ea67eb5b42add92f2ac22f4ef453f8711450
Karol Uracz <karol_ur@interia.pl>
2020-04-18 20:54:23 +0200

    admin classes

---
+++ admin/ExerciseAdmin.java
@@ -0,0 +1,63 @@
+package admin;
+
+import dao.ExerciseDao;
+import model.Exercise;
+
+import java.util.Scanner;
+
+public class ExerciseAdmin {
+    public static void main(String[] args) {
+        ExerciseDao exerciseDao = new ExerciseDao();
+        String option = null;
+        do {
+            Exercise[] exercises = new Exercise[0];
+            for (Exercise exercise: exercises) {
+                System.out.println(exercise);
+            }
+            System.out.print("Wybierz jedną z opcji: add, edit, delete, quit");
+            System.out.print("Select: ");
+            Scanner scanner = new Scanner(System.in);
+            while (!scanner.hasNextLine()){
+                System.out.print("Wprowadź poprawną opcję: ");
+                scanner.next();
+            }
+            option = scanner.nextLine();
+            switch (option) {
+                case "add":
+                    Exercise exercise = new Exercise();
+                    System.out.print("Wprowadź nowy tytuł zadania: ");
+                    String newTitle = scanner.next();
+                    System.out.println("Wprowadź nowy opis: ");
+                    String newDescription = scanner.next();
+                    exercise.setTitle(newTitle);
+                    exercise.setDescription(newDescription);
+                    break;
+                case "edit":
+                    System.out.print("Podaj id użytkownika do edycji: ");
+                    int exerciseId = scanner.nextInt();
+                    Exercise toUpdate = exerciseDao.readExercise(exerciseId);
+                    if (toUpdate == null) {
+                        System.out.println("ćwiczenie o takim id nie istnieje.");
+                        break;
+                    }
+                    System.out.println("Wprowadź nową nazwę zadania: ");
+                    String updateTitle = scanner.next();
+                    System.out.println("Wprowadź nowy opis: ");
+                    String updateDescription = scanner.next();
+                    toUpdate.setTitle(updateTitle);
+                    toUpdate.setDescription(updateDescription);
+                    exerciseDao.updateExercise(toUpdate);
+                    break;
+                case "delete":
+                    System.out.print("Podaj id grupy do usunięcia: ");
+                    exerciseId = scanner.nextInt();
+                    exerciseDao.deleteExercise(exerciseId);
+                    break;
+                case "quit":
+                default:
+                    break;
+            }
+
+        } while (!"quit".equalsIgnoreCase(option));
+    }
+}
